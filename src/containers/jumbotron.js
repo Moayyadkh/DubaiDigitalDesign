f5ad9012189532d0a77669896767a838916e0d19
Moayyadkh <31673360+Moayyadkh@users.noreply.github.com>
2021-05-09 13:00:33 +0300

    commit

--- src/containers/jumbotron.js
+++ src/containers/jumbotron.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import jumboData from '../fixtures/jumbo';
 import { Jumbotron } from '../components';
+import Video from "./Video";
 
 export function JumbotronContainer() {
   return (
@@ -13,7 +14,13 @@ export function JumbotronContainer() {
           </Jumbotron.Pane>
           <Jumbotron.Pane>
             <Jumbotron.Image src={item.image} alt={item.alt} />
+         </Jumbotron.Pane>
+
+         <Jumbotron.Pane>
+            <Jumbotron.Video src={item.Video} alt={item.alt} />
+          
           </Jumbotron.Pane>
+          
         </Jumbotron>
       ))}
     </Jumbotron.Container>
