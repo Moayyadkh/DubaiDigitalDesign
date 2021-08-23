f291057dbebf521824d8240d374657654e4dbaa5
Moayyadkh <31673360+Moayyadkh@users.noreply.github.com>
2021-08-23 19:16:30 +0300

    commit

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,6 +5,28 @@ import { JumbotronContainer } from '../containers/jumbotron';
 import { FaqsContainer } from '../containers/faqs';
 import { FooterContainer } from '../containers/footer';
 
+import React from "react";
+import PropTypes from "prop-types";
+
+const YoutubeEmbed = ({ embedId }) => (
+  <div className="video-responsive">
+    <iframe
+      width="853"
+      height="480"
+      src={`https://www.youtube.com/embed/${embedId}`}
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+      title="Embedded youtube"
+    />
+  </div>
+);
+
+YoutubeEmbed.propTypes = {
+  embedId: PropTypes.string.isRequired
+};
+
+
 //import { Header } from '../components';
 //import * as ROUTES from '../constants/routes';d
 
@@ -24,6 +46,8 @@ export default function Home() {
             
             <OptForm.Break />
             <OptForm.Text>Enter your email to Start your 3dsmax Online-LIVE Modeling Basics Course!.</OptForm.Text>*/}
+
+
           </OptForm>
         </Feature>
       </HeaderContainer>
