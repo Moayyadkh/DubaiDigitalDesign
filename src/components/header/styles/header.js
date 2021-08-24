5aadcd09c2770e22cd8170647d423d31a2b4e0ce
Moayyadkh <31673360+Moayyadkh@users.noreply.github.com>
2021-08-24 07:40:13 +0300

    commit

--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -229,4 +229,20 @@ export const PlayButton = styled.button `
     background-color: #ff1e1e;
     color: white;
   }
+
+
+.video-responsive {
+  overflow: hidden;
+  padding-bottom: 56.25%;
+  position: relative;
+  height: 0;
+}
+
+.video-responsive iframe {
+  left: 0;
+  top: 0;
+  height: 100%;
+  width: 100%;
+  position: absolute;
+}
 `;
