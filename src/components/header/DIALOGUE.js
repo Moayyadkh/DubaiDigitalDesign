5c422c5657e156fc7f5166f08ba2ee4832a3708f
Moayyadkh <31673360+Moayyadkh@users.noreply.github.com>
2021-08-25 12:44:40 +0300

    commit

--- src/components/header/DIALOGUE.js
+++ src/components/header/DIALOGUE.js
@@ -18,8 +18,8 @@ const DIALOGUE = ({ embedId }) => (
                                          
              src="https://console.dialogflow.com/api-client/demo/embedded/53dd1c22-2525-40bd-bb2f-f5d52c6ab69d">
 
-               
-        
+
+
         </iframe>
     </div>
   );
