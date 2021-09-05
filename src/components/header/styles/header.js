46ff99ac6238aceb39a47846315a74b4d072a733
Moayyadkh <31673360+Moayyadkh@users.noreply.github.com>
2021-09-05 09:12:47 +0300

    commit

--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -2,7 +2,7 @@ import styled from 'styled-components/macro';
 import { Link as ReachRouterLink } from 'react-router-dom';
 
 export const Background = styled.div `
-  display: flex;
+ display: flex;
   
   flex-direction: column;
   background: url(${({ src }) => (src ? `.. / images / misc / $ { src }.jpg ` : '../images/misc/home-bg.jpg')}) top left / cover
