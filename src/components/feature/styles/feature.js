43bc69ddd57fb96e51819a9fbb9c0a88512de2f1
Moayyadkh <31673360+Moayyadkh@users.noreply.github.com>
2021-09-15 09:55:44 +0300

    commit

--- src/components/feature/styles/feature.js
+++ src/components/feature/styles/feature.js
@@ -7,23 +7,7 @@ export const Container = styled.div`
   border-bottom: 8px solid #222;
   text-align: center;
   padding: 10px 45px;
-  .container {
-    position: relative;
-    width: 100%;
-    overflow: hidden;
-    padding-top: 56.25%; /* 16:9 Aspect Ratio */
-  }
-  
-  .responsive-iframe {
-    position: absolute;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
-    width: 100%;
-    height: 100%;
-    border: none;
-  }
+
   
 `;
 
