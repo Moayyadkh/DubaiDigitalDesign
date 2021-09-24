fe247ad026863b69fc39bf56cc977dbebf67425f
Moayyadkh <31673360+Moayyadkh@users.noreply.github.com>
2021-09-24 12:00:51 +0300

    commit

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Switch,Route } from 'react-router-dom';
 import { Home, Browse, SignIn, SignUp } from './pages';
 import * as ROUTES from './constants/routes';
 import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
 import { useAuthListener } from './hooks';
+
+
 
 export function App() {
   const { user } = useAuthListener();
@@ -14,14 +16,21 @@ export function App() {
         <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN}>
           <SignIn />
         </IsUserRedirect>
+        
         <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP}>
           <SignUp />
         </IsUserRedirect>
         <ProtectedRoute user={user} path={ROUTES.BROWSE}>
           <Browse />
         </ProtectedRoute>
+
+        <Route path="/" component = {Home}/>
+           <IsUserRedirect from='//' to="/3dstudio-max-vray-rendering-online-course-training/" />
+           <Route path="/tutorials/"/>
+
         <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME}>
           <Home />
+          
         </IsUserRedirect>
       </Switch>
     </Router>
