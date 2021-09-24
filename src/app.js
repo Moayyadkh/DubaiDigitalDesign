f1f71f58e7186873a8df0a7224626cf36d156a4b
Moayyadkh <31673360+Moayyadkh@users.noreply.github.com>
2021-09-24 12:50:22 +0300

    commit

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch,Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch,Route,Redirect } from 'react-router-dom';
 import { Home, Browse, SignIn, SignUp } from './pages';
 import * as ROUTES from './constants/routes';
 import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
@@ -25,9 +25,9 @@ export function App() {
         </ProtectedRoute>
 
         <Route path="/" component = {Home}/>
-           <IsUserRedirect from='//' to="/3dstudio-max-vray-rendering-online-course-training/" />
-           <Route path="/3dstudio-max-vray-rendering-online-course-training/"/>
-
+        <Redirect from='/' to ="/3dstudio-max-vray-rendering-online-course-trainin"/>
+       
+   
         <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME}>
           <Home />
           
