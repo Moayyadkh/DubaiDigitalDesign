c8d9ce5f525f5cebddc98b0c5e3566ed6d2f1efd
Moayyadkh <31673360+Moayyadkh@users.noreply.github.com>
2021-09-24 15:41:24 +0300

    commit

--- src/app.js
+++ src/app.js
@@ -24,6 +24,49 @@ export function App() {
           <Browse />
         </ProtectedRoute>
 
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME}>
+          < Home/>
+        </IsUserRedirect>
+       
+         
+        <Redirect from='//' to ="/3dstudio-max-vray-rendering-online-course-training/"/>
+       
+        <Route path="/3dstudio-max-vray-rendering-online-course-training/" component={Home}/>
+         
+        </Switch>
+    </Router>
+  );
+}
+
+/*
+
+import React from 'react';
+import { BrowserRouter as Router, Switch,Route,Redirect } from 'react-router-dom';
+import { Home, Browse, SignIn, SignUp } from './pages';
+import * as ROUTES from './constants/routes';
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
+import { useAuthListener } from './hooks';
+
+
+
+export function App() {
+  const { user } = useAuthListener();
+
+  return (
+    <Router>
+      <Switch>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN}>
+          <SignIn />
+        </IsUserRedirect>
+        
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP}>
+          <SignUp />
+        </IsUserRedirect>
+        <ProtectedRoute user={user} path={ROUTES.BROWSE}>
+          <Browse />
+        </ProtectedRoute>
+
+       
         <Route path="/" component = {Home}/>
         <Redirect from='//' to ="/3dstudio-max-vray-rendering-online-course-training/"/>
        
@@ -33,3 +76,4 @@ export function App() {
     </Router>
   );
 }
+*/
