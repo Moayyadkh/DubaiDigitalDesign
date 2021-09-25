9965c25a0bd89c0169248b1a560182847cbfa8c2
Moayyadkh <31673360+Moayyadkh@users.noreply.github.com>
2021-09-25 15:02:30 +0300

    commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-export { default as HOME } from   './3dstudio-max-vray-rendering-online-course-training';
+export { default as Home } from   './3dstudio-max-vray-rendering-online-course-training';
 export { default as Browse } from './browse';
 export { default as SignIn } from './signin';
 export { default as SignUp } from './signup';
