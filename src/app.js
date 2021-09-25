79366f260052e1edbb12214a1ea6f742c5834503
Moayyadkh <31673360+Moayyadkh@users.noreply.github.com>
2021-09-25 13:26:52 +0300

    commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch,Route,Redirect } from 'react-router-dom';
-import { HOME, Browse, SignIn, SignUp } from './pages';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Home, Browse, SignIn, SignUp } from './pages';
 import * as ROUTES from './constants/routes';
 import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
 import { useAuthListener } from './hooks';
-
-
 
 export function App() {
   const { user } = useAuthListener();
@@ -16,27 +14,20 @@ export function App() {
         <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN}>
           <SignIn />
         </IsUserRedirect>
-        
         <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP}>
           <SignUp />
         </IsUserRedirect>
         <ProtectedRoute user={user} path={ROUTES.BROWSE}>
           <Browse />
         </ProtectedRoute>
-
         <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME}>
-          < HOME/>
+          <Home />
         </IsUserRedirect>
-       
-         
-      
-
-        
-         
-        </Switch>
+      </Switch>
     </Router>
   );
 }
+
 
 /*
 
