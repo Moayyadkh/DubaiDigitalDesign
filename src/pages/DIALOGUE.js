a5dba637ccfec7b5aa7ca04c3a13de4cb5c9326e
Moayyadkh <31673360+Moayyadkh@users.noreply.github.com>
2021-11-04 18:00:20 +0200

    commit

--- src/pages/DIALOGUE.js
+++ src/pages/DIALOGUE.js
@@ -9,8 +9,8 @@ const DIALOGUE = ({ embedId }) => (
    <iframe 
      
     allow="microphone;"
-    width="653"
-    height="280"
+    width="753"
+    height="220"
     src="https://console.dialogflow.com/api-client/demo/embedded/53dd1c22-2525-40bd-bb2f-f5d52c6ab69d">
       
 </iframe>
