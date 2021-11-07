f65376e0207f8ad9a875ffeccf81544c4ed5433a
Moayyadkh <31673360+Moayyadkh@users.noreply.github.com>
2021-11-07 06:03:25 +0200

    commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 export { default as Home } from   './3dmax-vray-rendering-online-training';
 export { default as Browse } from './browse';
+//export { default as Browse } from './3DAssets';
 //export { default as SignIn } from './signin';
 //export { default as SignUp } from './signup';
