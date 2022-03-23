ae2d20fec423729aea96579d9fcb4c88da370c17
Moayyadkh <31673360+Moayyadkh@users.noreply.github.com>
2022-03-23 18:41:12 +0300

    commit

--- src/pages/YoutubeEmbed.js
+++ src/pages/YoutubeEmbed.js
@@ -2,7 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 //import "./styles.css";
 
-
 const YoutubeEmbed = ({ embedId }) => (
   <div className="video-responsive">
     <iframe
