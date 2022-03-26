6e6d4a39ece621a3e393ff38bcbc49507b7ca921
Moayyadkh <31673360+Moayyadkh@users.noreply.github.com>
2022-03-26 20:28:07 +0300

    commit

--- src/containers/footer.js
+++ src/containers/footer.js
@@ -14,9 +14,9 @@ export function FooterContainer() {
 
            <iframe
     allow="microphone;"
+    
     width="100%"
-     
-    height="430"
+    height="400"
     src="https://console.dialogflow.com/api-client/demo/embedded/53dd1c22-2525-40bd-bb2f-f5d52c6ab69d">
 </iframe>
       
